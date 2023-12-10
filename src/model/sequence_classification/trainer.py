baed3b0a9ef76cdfa7fa90d6b19f50bc0b26e41b
peulsilva <peulsilva9@gmail.com>
2023-12-10 16:11:02 +0100

    add ag_news_analysis (bert)

---
+++ src/model/sequence_classification/trainer.py
@@ -0,0 +1,111 @@
+import torch
+import logging
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from src.preprocessing.make_dataset import SplitWordsDataset
+from typing import Dict
+from torch.nn.functional import cross_entropy
+from transformers import AdamW
+from torcheval.metrics.functional import multiclass_f1_score, multiclass_confusion_matrix
+from copy import deepcopy
+from transformers import AutoModelForSequenceClassification
+
+class SequenceClassificationTrainer:
+    def __init__(
+        self, 
+        model : AutoModelForSequenceClassification,
+        num_classes : int,
+        device : str = 'cuda',
+    ) -> None:
+        
+        self.model = model
+        self.num_classes = num_classes
+
+        self.device = device
+
+    def compile(
+        self,
+        train_dataloader : DataLoader,
+        val_dataloader : DataLoader,
+        n_epochs : int = 10,
+        lr : float = 1e-5
+    ):
+        optimizer = torch.optim.AdamW(
+            self.model.parameters(),
+            lr = lr
+        )
+
+        self.history = {
+            'val_f1' : [],
+            'conf_matrix': None
+        }
+
+        best_f1 = 0
+        self.best_model = None
+
+        logging.info(f"Training model with {len(train_dataloader.dataset)//4} shots")
+
+        for epoch in range(n_epochs):
+            for batch in tqdm(train_dataloader):
+
+                for k,v in batch.items():
+                    batch[k] = v.to(self.device)
+
+                optimizer.zero_grad()
+
+                output = self.model(**batch)
+                loss = output['loss']
+
+                loss.backward()
+                optimizer.step()
+
+
+            y_true_val = torch.tensor([],device=self.device)
+            y_pred_val = torch.tensor([],device=self.device)
+            
+            for batch_val in val_dataloader:
+
+                for k,v in batch_val.items():
+                    batch_val[k] = v.to(self.device)
+
+                with torch.no_grad():
+                    out = self.model(**batch_val)
+
+                    y_pred = out.logits.softmax(dim = 1)
+                    y_true = batch_val['labels']
+
+                    y_pred_val = torch.cat([
+                        y_pred_val, 
+                        torch.tensor(y_pred).to(self.device)
+                    ])
+
+                    y_true_val = torch.cat([
+                        y_true_val, 
+                        torch.tensor(y_true).to(self.device)
+                    ])
+
+            f1 = multiclass_f1_score(
+                y_pred_val.argmax(dim = 1).to(torch.int64),
+                y_true_val.to(torch.int64),
+                num_classes= self.num_classes
+            )
+
+            conf_matrix = multiclass_confusion_matrix(
+                y_pred_val.argmax(dim = 1).to(torch.int64), 
+                y_true_val.to(torch.int64),
+                num_classes=self.num_classes
+            )
+
+            logging.info(f"f1: {f1}")
+            logging.info(conf_matrix)
+
+            self.history['val_f1'].append(f1.item())
+
+            if f1 > best_f1:
+                best_f1 = f1
+                self.best_model = deepcopy(self.model)
+                self.history['val_conf_matrix'] = conf_matrix
+        
+        return self.history
+
+        
---
+++ src/preprocessing/sequence_classification/dataset.py
@@ -0,0 +1,83 @@
+import torch 
+from torch.utils.data import Dataset, DataLoader
+from typing import List, Dict
+from transformers import AutoTokenizer
+
+class TextDataset(Dataset):
+    def __init__(self, 
+                 input_ids : torch.Tensor, 
+                 attention_mask : torch.Tensor,
+                 labels : torch.tensor, 
+                ) -> None:
+        super().__init__()
+
+        self.input_ids = input_ids
+        self.labels = labels 
+        self.attention_mask = attention_mask
+
+    def __len__(self):
+        return self.input_ids.shape[0]
+    
+    def __getitem__(self, index):
+        return {
+            "input_ids": self.input_ids[index],
+            "attention_mask": self.attention_mask[index],
+            "labels": self.labels[index]
+        }
+    
+
+def get_dataloader(
+    text : List[str],
+    labels : List[str],
+    tokenizer : AutoTokenizer,
+    n_shots : int,
+    num_classes : int,
+    **kwargs
+):
+    """Returns the dataloader with the correct number of shots per label
+
+    Args:
+        data_per_label (Dict): _description_
+        tokenizer (AutoTokenizer): _description_
+        n_shots (int): _description_
+
+    Returns:
+        _type_: _description_
+    """    
+    data_per_label = {}
+
+    for i in range(num_classes):
+        data_per_label[i] = []
+
+    for i in range(len(text)):
+        data_per_label[labels[i]].append(text[i])
+
+    text = []
+    labels = []
+    num_classes = len(data_per_label.keys())
+    
+    for label in range(num_classes):
+        for j in range(n_shots):
+            text.append(data_per_label[label][j])
+            labels.append(label)
+            
+    tokens = tokenizer(
+        text,
+        truncation= True,
+        padding= "max_length",
+        return_tensors= "pt",
+        max_length=256,
+    )
+
+    dataset = TextDataset(
+        input_ids=tokens['input_ids'],
+        attention_mask=tokens['attention_mask'],
+        labels = torch.tensor(labels)
+    )
+
+    dataloader = DataLoader(
+        dataset,
+        **kwargs
+    )
+
+    return dataloader
