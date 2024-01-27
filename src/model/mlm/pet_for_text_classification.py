7f853ec4df97f20ccd156eeaac33d6fb7e3b8c38
peulsilva <peulsilva9@gmail.com>
2024-01-27 11:20:17 +0100

    add PET for text classification

---
+++ src/model/mlm/pet_for_text_classification.py
@@ -0,0 +1,134 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+from typing import Dict
+from tqdm import tqdm
+import logging
+from torcheval.metrics.functional import multiclass_f1_score, multiclass_confusion_matrix
+from torch.nn.functional import cross_entropy
+
+
+def get_y_true(
+    input: Dict[str, torch.Tensor],
+    inverse_verbalizer : Dict,
+    device : str = "cuda",
+)-> torch.tensor:
+    
+    y_true = input['labels']
+    y_true = y_true[y_true!= -100].item()
+    y_true = inverse_verbalizer[y_true]
+    
+    return torch.tensor(y_true, device= device)
+
+def train(
+    train_dataloader : DataLoader,
+    val_dataloader : DataLoader,
+    num_classes: int,
+    model : AutoModelForMaskedLM,
+    alpha : float,
+    verbalizer : Dict,
+    tokenizer : AutoTokenizer,
+    device : str = 'cuda',
+    lr : float = 1e-5
+):
+    inverse_verbalizer = {v:k for k, v in verbalizer.items()}
+
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr = lr
+    )
+
+
+    n_epochs = 10
+
+    best_f1=  0
+    confusion_matrix= None
+
+    history = []
+
+    for epoch in range(n_epochs):
+
+        for input in train_dataloader:
+            out = model(**input)
+
+            loss_mlm = out['loss']
+
+            y_true = get_y_true(
+                input,
+                inverse_verbalizer
+            )
+
+            mask_token_index = torch.where(input["input_ids"] == tokenizer.mask_token_id)[1]
+            mask_token_logits = out.logits[0, mask_token_index, :]
+            
+            predictions = torch.Tensor(
+                [mask_token_logits[0,x] for x in verbalizer.values()]
+            )
+
+            probabilities = predictions.softmax(dim = 0).to(device)
+
+            loss_ce = cross_entropy(
+                probabilities,
+                y_true
+            )
+            loss_ce.requires_grad = True
+
+            loss = alpha*loss_mlm + (1-alpha)* loss_ce
+            
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+        with torch.no_grad():    
+            y_true_val = torch.tensor([],device=device)
+            y_pred_val = torch.tensor([],device=device)
+
+            for input in tqdm(val_dataloader):
+                out = model(**input)
+
+                y_true = get_y_true(input)
+
+                mask_token_index = torch.where(input["input_ids"] == tokenizer.mask_token_id)[1]
+                mask_token_logits = out.logits[0, mask_token_index, :]
+                
+                predictions = torch.Tensor(
+                    [mask_token_logits[0,x] for x in verbalizer.values()]
+                )
+
+                y_pred = predictions.argmax().to(device)
+
+                y_pred_val = torch.cat([
+                    y_pred_val, 
+                    torch.tensor([y_pred]).to(device)
+                ])
+
+                y_true_val = torch.cat([
+                    y_true_val,
+                    torch.tensor([y_true]).to(device)
+                ])
+            
+            f1 = multiclass_f1_score(
+                y_pred_val,
+                y_true_val,
+                num_classes= num_classes
+            )
+
+
+            conf_matrix = multiclass_confusion_matrix(
+                y_pred_val.to(torch.int64),
+                y_true_val.to(torch.int64),
+                num_classes= num_classes
+            )
+
+            if f1 > best_f1:
+                best_f1 = f1
+                confusion_matrix = conf_matrix
+
+            history.append(f1.item())
+
+            logging.info("-------------------------")
+            logging.info(f"End of epoch {epoch}: f1 = {f1.item()}")
+            logging.info(conf_matrix)
+    
+    return history, confusion_matrix
---
+++ src/preprocessing/pet/pet_for_text_classification_dataset.py
@@ -0,0 +1,44 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import AutoModelForMaskedLM, AutoTokenizer
+from typing import Dict, List
+from copy import deepcopy
+
+class PETDatasetForClassification(Dataset):
+    def __init__(
+        self, 
+        processed_text : List[str], 
+        labels : List[int],
+        tokenizer : AutoTokenizer,
+        device : str = "cuda"
+    ) -> None:
+        super().__init__()
+
+        self.tokens = tokenizer(
+            processed_text,
+            return_tensors="pt",
+            padding="max_length",
+            truncation=True
+        )
+        self.encoded_labels = deepcopy(self.tokens['input_ids'])
+        
+        self.encoded_labels[self.encoded_labels != tokenizer.mask_token_id] = -100
+
+        for idx, sentence in enumerate(self.encoded_labels):
+            sentence[sentence == tokenizer.mask_token_id] = tokenizer.vocab[labels[idx].lower()]
+
+        self.inputs : Dict[str, torch.Tensor] = self.tokens
+        self.inputs['labels'] = self.encoded_labels
+
+        for k,v in self.inputs.items():
+            self.inputs[k] = v.to(device)
+
+    def __getitem__(self, index):
+        d : Dict = dict()
+        for key in self.inputs.keys():
+            d[key] = self.inputs[key][index]
+
+        return d
+    
+    def __len__(self):
+        return self.tokens['input_ids'].shape[0]
